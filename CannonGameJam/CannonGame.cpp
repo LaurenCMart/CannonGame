7d4cf5cc844502c2b88db7852af0487d14c50099
Lauren Martin <laurencmart@gmail.com>
2017-01-23 16:46:53 +0000

    Initial Commit. Setup.

---
+++ CannonGameJam/CannonGame.cpp
@@ -0,0 +1,22 @@
+/* To Do:
+1. Create window, add background.
+2. Create a rectangle for the ground.
+3. Create Cannon
+4. Make cannon move
+5. Make cannon fire projectile
+6. Add some wall obsticles
+7. Add a target for the cannon ball
+8. When target is destroyed, target disappears. Game Won message appears.
+9. Add 3 balls for player. When all 3 cannon balls have been used, and target hasn't been hit, game over message appears.
+10. After either game over or game won messages appear, ask user if they want to play again.
+11. Add sounds and background music.
+*/
+
+#include "SDL.h"
+
+int main(int argc, char**argv)
+{
+    SDL_ShowSimpleMessageBox(0, "Hello World", "Yer Maw", 0);
+
+    return 0;
+}
